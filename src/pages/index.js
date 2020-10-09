eed4ae01e6d9321128ddceffbf2333e2f316c748
Jaimee-Ian Rodriguez <Jaimee-Ian.rodriguez@macaulay.cuny.edu>
2020-10-08 23:10:25 -0400

    created copy file and edited index file to link to copy file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div><a href="/copy">Check out this page!</a></div>
 }
