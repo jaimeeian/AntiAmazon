00d83862f65ba3f423146177e022ab90eab4ef0a
rxnaij <richardblue37@gmail.com>
2020-11-06 19:31:26 -0500

    config gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,17 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require('dotenv').config()
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
