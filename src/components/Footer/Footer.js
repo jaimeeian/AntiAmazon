b8d4b353a0c832e90f208817219119f6daef0236
jaimeeian <jaimee-ian.rodriguez@macaulay.cuny.edu>
2021-01-06 19:55:31 -0500

    fixed link colors and background

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,18 @@ import { graphql, Link, useStaticQuery } from "gatsby"
 
 export default function Footer() {
     return(
-        <div className="bg-primary text-white">
-            <p className="font-bold">Anti-Amazon shopper</p>
+        <div className="bg-blue text-white">
+            <ul className="list-none">
+                <li className="font-bold">Anti-Amazon shopper</li>
 
-            <p><Link to='/' className="underline hover:text-blue-500">About us</Link></p>
+                <li><Link to='/' className="underline hover:text-blue-500">About us</Link></li>
 
-            <p><Link to='/' className="underline hover:text-blue-500">The Case against Amazon</Link></p>
-            
-            <p>created by xxx 2020</p>
+                <li><Link to='/' className="underline hover:text-blue-500">The Case against Amazon</Link></li>
+                
+                <li>created by xxx 2020</li>
 
-            <p><Link to='/' className="underline hover:text-blue-500">Github</Link></p>
+                <li><Link to='/' className="underline hover:text-blue-500">Github</Link></li>
+            </ul>
         </div>
 
     )
