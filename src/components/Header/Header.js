bbf892271d26b78c6b31510e4d202a8fb657ea4b
edrick <edrickl25@gmail.com>
2021-01-06 19:30:07 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-export default function Header() {    
+export default function Header() {
+    const [menuIsOpen,setMenu] = React.useState(false)   
     return (
     <div className="bg-gray-50 font-semibold py-4">
         <span>Anti-Amazon Shopper</span>
         <nav>
-            <ul className="list-outside font-light">
+            <button className={"inline-block"} onClick = {() => setMenu(!menuIsOpen)}>
+                toggle menu
+            </button>
+            <ul className={menuIsOpen? "list-outside font-light" : "hidden"}>
                 <li>Home</li>
                 <li>Why not Amazon?</li>
                 <li className="bg-red-500 text-white font-medium bg-opacity-75">Download the extension</li>
