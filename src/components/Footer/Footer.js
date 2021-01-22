c5b023f3c067f219e22830f6d4a49e1f3aa8bb10
jaimeeian <jaimee-ian.rodriguez@macaulay.cuny.edu>
2021-01-21 19:30:19 -0500

    added line breaks between text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
     return(
         <div className="bg-gray-700 text-white">
             <ul className="list-none p-8">
-                <li className="font-bold">Anti-Amazon shopper</li>
+                <li className="font-bold mb-4">Anti-Amazon shopper</li>
 
-                <li className='space-y-8'><Link to='/' className="underline hover:text-blue-500">About us</Link></li>
+                <li><Link to='/' className="underline hover:text-blue-500">About us</Link></li>
 
-                <li><Link to='/' className="underline hover:text-blue-500">The Case against Amazon</Link></li>
+                <li className="mb-4"><Link to='/' className="underline hover:text-blue-500">The Case against Amazon</Link></li>
                 
                 <li>created by xxx 2020</li>
 
