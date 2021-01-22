d945e1648e4ca76bc9743db98c357b566ffc4fd3
jaimeeian <jaimee-ian.rodriguez@macaulay.cuny.edu>
2021-01-21 19:26:39 -0500

    added padding and darkened background

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,11 @@ import { graphql, Link, useStaticQuery } from "gatsby"
 
 export default function Footer() {
     return(
-        <div className="bg-gray-100 text-white">
-            <ul className="list-none">
+        <div className="bg-gray-700 text-white">
+            <ul className="list-none p-8">
                 <li className="font-bold">Anti-Amazon shopper</li>
 
-                <li><Link to='/' className="underline hover:text-blue-500">About us</Link></li>
+                <li className='space-y-8'><Link to='/' className="underline hover:text-blue-500">About us</Link></li>
 
                 <li><Link to='/' className="underline hover:text-blue-500">The Case against Amazon</Link></li>
                 
