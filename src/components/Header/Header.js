541d855742244c6286bdd14737e59dfb2f139956
edrick <edrickl25@gmail.com>
2021-02-25 20:05:03 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,100 +1,52 @@
 import React from "react";
-
+import { Link } from "gatsby";
 export default function Header() {
     const [menuIsOpen, setMenu] = React.useState(false)
     return (
         <div className="bg-gray-200 font-semibold py-4">
-            <nav class>
-                <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                    <div class="relative flex items-center justify-between h-16">
-                        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            {/* Mobile menu button*/}
-                                <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false" onClick={() => setMenu(!menuIsOpen)} >
-                                <span class="sr-only">Open main menu</span>
-                                {/* Icon when menu is closed. */}
-          {/*
-            Heroicon name: menu
-
-            Menu open: "hidden", Menu closed: "block"
-          */}
-                                <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <nav>
+                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex items-center justify-between h-16">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:block md:hidden">
+                            <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false" onClick={() => setMenu(!menuIsOpen)} >
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                                 </svg>
-                                {/* Icon when menu is open. */}
-          {/*
-            Heroicon name: x
-
-            Menu open: "block", Menu closed: "hidden"
-          */}
-                                <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
                         </div>
-                        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                            <div class="flex-shrink-0 flex items-center">
+                        <div className="flex-1 flex items-center sm:justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex-shrink-0 flex items-center">
                                 <span>Anti-Amazon Shopper</span>
-                              {/*<img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/>
-                                <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"/> */}
                             </div>
-
-                    </div>
-                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span class="sr-only">View notifications</span>
-                                {/* Heroicon name: bell */}
-                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                            </svg>
-                            </button>
-
-                            {/* Profile dropdown */}
-                            <div class="ml-3 relative">
+                        </div>
+                        <div className="flex flex-row space-y-4 hidden md:block">
+                            <Link to="#" className="px-3 py-2 font-semibold text-sm font-medium border-b-2 border-black">Home</Link>
+                            <Link to="#" className="text-black hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Why not Amazon?</Link>
+                            <Link to="#" className="bg-red-500 text-white font-medium bg-opacity-75 hover:bg-red-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Download the extension</Link>
+                        </div>
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <div className="ml-3 relative">
                                 <div>
-                                    <button class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
-                                        <span class="sr-only">Open user menu</span>
-                                        {/*<img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/> */}
+                                    <button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
+                                        <span className="sr-only">Open user menu</span>
                                     </button>
                                 </div>
-                {/*
-            Profile dropdown panel, show/hide based on dropdown state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          */}
                             </div>
                         </div>
                     </div>
                 </div>
-
-{/*
-    Mobile menu, toggle classes based on menu state.
-
-    Menu open: "block", Menu closed: "hidden"
-  */}
-                <div class= {menuIsOpen ? "sm:block sm:ml-6" : "hidden md:visible"}>
-                    <div class="flex flex-col space-y-4">
-                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                        <a href="#" class="px-3 py-2 font-semibold text-sm font-medium border-b-2 border-black">Home</a>
-                        <a href="#" class="text-black hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Why not Amazon?</a>
-                        <a href="#" class="bg-red-500 text-white font-medium bg-opacity-75 hover:bg-red-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Download the extension</a>
+                <div className={menuIsOpen ? "md:hidden sm:block sm:ml-6" : "hidden"}>
+                    <div className="flex flex-col space-y-4">
+                        <Link to="#" className="px-3 py-2 font-semibold text-sm font-medium border-b-2 border-black">Home</Link>
+                        <Link to="#" className="text-black hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Why not Amazon?</Link>
+                        <Link to="#" className="bg-red-500 text-white font-medium bg-opacity-75 hover:bg-red-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Download the extension</Link>
                     </div>
                 </div>
-
-           {/*} <button className="inline-block inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={() => setMenu(!menuIsOpen)}>
-                    toggle menu
-                </button>
-                <ul className={menuIsOpen ? "list-outside font-light list-none" : "hidden"}>
-                    <li>Home</li>
-                    <li>Why not Amazon?</li>
-                    <li className="bg-red-500 text-white font-medium bg-opacity-75">Download the extension</li>
-                </ul> */}
             </nav>
         </div>
-    
-)
+    )
 }
